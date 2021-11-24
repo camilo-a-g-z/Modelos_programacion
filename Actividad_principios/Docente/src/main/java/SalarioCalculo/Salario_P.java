a5da768cd445f8799fd69781ca94398c4961338c
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2021-11-24 13:03:59 -0500

    Se cambian comentarios y se reduce cantidad de lineas de codigo implementado

--- Actividad_principios/Docente/src/main/java/SalarioCalculo/Salario_P.java
+++ Actividad_principios/Docente/src/main/java/SalarioCalculo/Salario_P.java
@@ -6,9 +6,7 @@ import java.io.InputStreamReader;
 
 public class Salario_P {
 	public static void main(String[] arg) throws IOException{
-		//mensaje de bienvenida
-		System.out.println("Bienvenido al programa que calcula salario de docente.");
-		System.out.println("");
+		System.out.println("Bienvenido al programa que calcula salario de docente.\n");
 		BufferedReader in = new BufferedReader(new InputStreamReader (System.in));
 		//se crean variables
 		int cantidad_docentes;
@@ -16,7 +14,6 @@ public class Salario_P {
 		int opcion;
 		boolean comprobar = true;
 		String desicion = "";
-		
 		do {
 			//se refresca valor de salario_t
 			salario_t=0.0;
@@ -29,62 +26,49 @@ public class Salario_P {
 			for(int i=0;i<cantidad_docentes;i++) {
                             //Se indica en que docente va
                             System.out.println("Docente "+(i+1));
-                            //bucle para vvalidar tipo de dato
+                            //bucle para validar tipo de dato
                             do {
                                     //se pregunta nivel de estudio
-                                    System.out.println("");
-                                    System.out.println("Nivel de estudio: 1)Doctorado\t2)Maestria\t3)Espesialisaci�n\t4)Sin postgrado");
+                                    System.out.println("\nNivel de estudio: 1)Doctorado\t2)Maestria\t3)Espesialisaci�n\t4)Sin postgrado");
                                     System.out.println("Opcion: ");
                                     opcion = Integer.parseInt(in.readLine());
                                     if(opcion>4||opcion<1) {
                                             System.out.println("Opcion invalida.");
                                     }
                             }while(opcion>4||opcion<1);
-                            //se calcula segun opcion
+                            //se calcula segun opcion, se crea tipo de docente en especifico y se almacena en array
                             switch (opcion) {
                             case 1:
-                                    //se crea nuevo docente
                                     Docente docente_d = new Doctorado();
-                                    //se pasa el nuevo docente al arreglo
                                     docentes[i] = docente_d;
                                     break;
                             case 2:
-                                    //se crea nuevo docente
                                     Docente docente_m = new Maestria();
-                                    //se pasa el nuevo docente al arreglo
                                     docentes[i] = docente_m;
                                     break;
                             case 3:
-                                    //se crea nuevo docente
                                     Docente docente_e = new Espesializacion();
-                                    //se pasa el nuevo docente al arreglo
                                     docentes[i] = docente_e;
                                     break;
                             case 4:
-                                    //se crea nuevo docente
                                     Docente docente_s_p = new S_Postgrado();
-                                    //se pasa el nuevo docente al arreglo
                                     docentes[i] = docente_s_p;
                                     break;
                             }
                             //se piden datos
                             docentes[i].pedir_datos();
-                            //se pregunta tip de contratacion
+                            //se pregunta tip de contratación
                             docentes[i].tipo_contratacion();
                             salario_t += docentes[i].getSalario();
 			}
-			System.out.println("");
-			System.out.println("Mostrando informacion de docentes:");
-			//se muestran docentes e informacion
+			System.out.println("\nMostrando informacion de docentes:");
+			//se muestran docentes e información
 			for(int i=0;i<cantidad_docentes;i++) {
-				//se muestran los datos
 				System.out.println("");
 				docentes[i].mostrar_datos();
 			}
 			//se muestra el salario total
-			System.out.println("");
-			System.out.println("El salario total a pagar por totos los maestros es: "+salario_t);
-			
+			System.out.println("\nEl salario total a pagar por totos los maestros es: "+salario_t);
 			//condicional para preguntar y verificar buclo do while
 			do {
 				System.out.println("Desea hacer otro calculo? (y/n): ");
@@ -97,10 +81,6 @@ public class Salario_P {
 				comprobar = false;
 			}
 		}while(comprobar);
-		
-		System.out.println("");
-		//mensaje de despedida
-		System.out.println("Gracias por implementar nuestro software");
+		System.out.println("\nGracias por implementar nuestro software");
 	}
 }
-
